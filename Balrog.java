4388356fb131c4e719abab6661a3d276db922be5
TheCraftyCat <70973861+TheCraftyCat@users.noreply.github.com>
2020-11-01 10:37:35 -0500

    Class Placeholders
    Literally just placeholders for each class before I switch computers (they won't compile at this version)

---
+++ Balrog.java
@@ -0,0 +1,77 @@
+
+/**
+ * Write a description of class Balrog here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Balrog extends Demon
+{
+    // instance variables - replace the example below with your own
+    private static final int MAX_ELF_HP = 25;
+    private static final int MIN_ELF_HP = 8;
+    private static final int MAX_ELF_STR = 18;
+    private static final int MIN_ELF_STR = 5;
+
+    /**
+     * Constructor for objects of class Elf -
+     * Note that the calling class does not need to know anything about the 
+     * requirements of elf minimum and maximum values
+     * 
+     * The instantiating class asks for an Elf and the Elf class is responsible for
+     * return an Elf object with values in the appropriate range
+     * 
+     */
+    public Balrog()
+    {
+        // note how the class uses the static randomizer class to
+        // generate the values. This localizes the need to know 
+        // max and min values to this class only
+        // max-min is range of values
+        // range + min ensures that the values don't start at one.
+        super(
+            Randomizer.nextInt(MAX_ELF_HP-MIN_ELF_HP)+MIN_ELF_HP,    
+            Randomizer.nextInt(MAX_ELF_STR-MIN_ELF_STR)+MIN_ELF_STR
+        );
+          
+    }
+    
+    
+    // attack() - not overridden because Humans generate basic damage
+    // takeDamage(int) - not overridden, because Humans take all damage assigned to them
+
+    
+}
+
+/**
+ * Write a description of class Balrog here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Balrog
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Balrog
+     */
+    public Balrog()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
---
+++ CyberDemon.java
@@ -0,0 +1,44 @@
+
+/**
+ * Write a description of class CyberDemon here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class CyberDemon extends Demon
+{
+    // instance variables - replace the example below with your own
+    private static final int MAX_ELF_HP = 25;
+    private static final int MIN_ELF_HP = 8;
+    private static final int MAX_ELF_STR = 18;
+    private static final int MIN_ELF_STR = 5;
+
+    /**
+     * Constructor for objects of class Elf -
+     * Note that the calling class does not need to know anything about the 
+     * requirements of elf minimum and maximum values
+     * 
+     * The instantiating class asks for an Elf and the Elf class is responsible for
+     * return an Elf object with values in the appropriate range
+     * 
+     */
+    public CyberDemon()
+    {
+        // note how the class uses the static randomizer class to
+        // generate the values. This localizes the need to know 
+        // max and min values to this class only
+        // max-min is range of values
+        // range + min ensures that the values don't start at one.
+        super(
+            Randomizer.nextInt(MAX_ELF_HP-MIN_ELF_HP)+MIN_ELF_HP,    
+            Randomizer.nextInt(MAX_ELF_STR-MIN_ELF_STR)+MIN_ELF_STR
+        );
+          
+    }
+    
+    
+    // attack() - not overridden because Humans generate basic damage
+    // takeDamage(int) - not overridden, because Humans take all damage assigned to them
+
+    
+}
